1d1a8c20a1015590a1f1c8ee6598b6ffb9972dc5
swathidaram9 <swathi.daram9@gmail.com>
2020-02-01 17:14:44 -0800

    update

--- src/main/java/com/project/assignment/controller/DocumentStorage.java
+++ src/main/java/com/project/assignment/controller/DocumentStorage.java
@@ -17,21 +17,18 @@ public class DocumentStorage {
         this.documentStorageService = documentStorageService;
     }
 
-    //Create - POST /storage/documents
     @PostMapping(value = "/documents", produces = "text/plain;charset=us-ascii")
     @ResponseStatus(HttpStatus.CREATED)
     public String createDocumentForStorage(@RequestBody String documentContent) {
         return documentStorageService.saveDocumentDetails(documentContent);
     }
 
-    //Query - GET /storage/documents/{docId}
     @GetMapping(value = "/documents/{docId}", produces = "text/plain;charset=us-ascii")
     @ResponseStatus(HttpStatus.OK)
     public String getDocument(@PathVariable String docId) {
         return documentStorageService.getDocumentDetails(docId);
     }
 
-    //Update - PUT /storage/documents/{docId}
     @PutMapping("/documents/{docId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateStoredDocument(@PathVariable String docId, @RequestBody String updatedDocumentContent) {
@@ -41,19 +38,18 @@ public class DocumentStorage {
         documentStorageService.updateStoredDocument(docId, updatedDocumentContent);
     }
 
-    //Delete - DELETE /storage/documents/{docId}
     @DeleteMapping("/documents/{docId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteStoredDocument(@PathVariable String docId) {
         // Check for document exists or not.
         documentStorageService.getDocumentDetails(docId);
-        // Delete the document.
+        // Delete the document from the DB.
         documentStorageService.deleteStoredDocumentDetails(docId);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(DocumentNotFoundException.class)
     public void handleException (DocumentNotFoundException exception) {
-        // TODO: Write the logic.
+        // TODO: This needs to be implemented.
     }
 }
